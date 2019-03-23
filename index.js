68c44c9bc9b5aeea461c5138c4ea25d3b93eac88
James Merkin <jnmerk@gmail.com>
2019-03-23 19:58:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// write your code below!
+// write your code below
+
+function happyHolidays() {
+return ("Happy holidays!") 
+  }
+  
+function happyHolidaysTo(name){
+  var person = name
+//  return ('Happy holidays,' person !)
+}
